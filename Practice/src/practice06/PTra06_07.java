06195af26588e0082fa09761f467047d91b817a3
DESKTOP-F9JJO7F\Yin Masaki <yinzhengqi446@gmail.com>
2017-09-26 17:11:11 +0900

    プラクティス6

--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -25,5 +25,44 @@ public class PTra06_07 {
 		 * ■□□□□
 		 */
 
-	}
-}
+        	 for (int j = 0; j < 10; j++) {
+
+	            	switch(j) {
+	            	case 0:
+	            		System.out.println("□□□□□");
+	            	  break;
+	            	case 1:
+	            		System.out.println("■□□□□");
+	            		break;
+	            	case 2:
+	            		System.out.println("■■□□□");
+	            		break;
+	            	case 3:
+	            		System.out.println("■■■□□");
+	            		break;
+	            	case 4:
+	            		System.out.println("■■■■□");
+	            		break;
+	            	case 5:
+	            		System.out.println("■■■■■");
+	            		break;
+	            	case 6:
+	            		System.out.println("■■■■□");
+	            		break;
+	            	case 7:
+	            		System.out.println("■■■□□");
+	            		break;
+	            	case 8:
+	            		System.out.println("■■□□□");
+	            		break;
+	            	case 9:
+	            		System.out.println("■□□□□");
+	            		break;
+	            	}
+
+	            }
+
+        	 }
+         }
+
+
