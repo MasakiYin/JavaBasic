29760a005faee28a054a0e9f41c27093c88238f2
DESKTOP-F9JJO7F\Yin Masaki <yinzhengqi446@gmail.com>
2017-09-27 19:08:38 +0900

    komitt

--- Practice/src/practice08/PTra08_06.java
+++ Practice/src/practice08/PTra08_06.java
@@ -28,13 +28,18 @@ public class PTra08_06 {
 	public static void dispValue(String message) {
 		System.out.println("「" + message + "」");
 	}
-
 	// ★ dispValueを引数intでオーバーロードしてください。処理は同じ
-
+     public static void dispValue(int i) {
+    	 System.out.println("「" + i + "」");
+     }
 
 	// ★ dispValueを引数doubleでオーバーロードしてください。処理は同じ
-
+     public static void dispValue(double i) {
+    	 System.out.println("「" + i + "」");
+     }
 
 	// ★ dispValueを引数charでオーバーロードしてください。処理は同じ
-
+     public static void dispValue(char i) {
+    	 System.out.println("「" + i + "」");
+     }
 }
